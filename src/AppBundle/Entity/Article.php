b6a862d50d34c6f72cf56e0c511fc937d0ba2b5f
tomming <233238526@qq.com>
2018-03-17 18:01:57 +0800

    初始配置后台及用户登录

---
+++ src/AppBundle/Entity/Article.php
@@ -0,0 +1,294 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Article
+ *
+ * @ORM\Table(name="article")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\ArticleRepository")
+ */
+class Article
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=200)
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="summary", type="string", length=255, nullable=true)
+     */
+    private $summary;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="titlePic", type="string", nullable=true)
+     *
+     */
+    private $titlePic;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="content", type="text")
+     */
+    private $content;
+
+    /**
+     * @var User
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\User")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $author;
+
+    /**
+     * @var Category[] | ArrayCollection
+     *
+     * @ORM\ManyToMany(targetEntity="AppBundle\Entity\Category", cascade={"persist"})
+     * @ORM\JoinTable(name="article_category")
+     * @ORM\OrderBy({"title":"ASC"})
+     * @Assert\Count(max=4,maxMessage="太多标签,最多只能添加4个")
+     */
+    private $categories;
+
+    /**
+     * @var \DateTime
+     * @ORM\Column(type="datetime")
+     * Assert\DateTime
+     */
+    private $createdAt;
+
+    /**
+     * @var \DateTime
+     * @ORM\Column(type="datetime")
+     * Assert\DateTime
+     */
+    private $updatedAt;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return Article
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set summary
+     *
+     * @param string $summary
+     *
+     * @return Article
+     */
+    public function setSummary($summary)
+    {
+        $this->summary = $summary;
+
+        return $this;
+    }
+
+    /**
+     * Get summary
+     *
+     * @return string
+     */
+    public function getSummary()
+    {
+        return $this->summary;
+    }
+
+    /**
+     * Set content
+     *
+     * @param string $content
+     *
+     * @return Article
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get content
+     *
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->categories = new ArrayCollection();
+    }
+
+    /**
+     * Set titlePic
+     *
+     * @param string $titlePic
+     *
+     * @return Article
+     */
+    public function setTitlePic($titlePic)
+    {
+        $this->titlePic = $titlePic;
+
+        return $this;
+    }
+
+    /**
+     * Get titlePic
+     *
+     * @return string
+     */
+    public function getTitlePic()
+    {
+        return $this->titlePic;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     *
+     * @return Article
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set updatedAt
+     *
+     * @param \DateTime $updatedAt
+     *
+     * @return Article
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get updatedAt
+     *
+     * @return \DateTime
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * Set author
+     *
+     * @param User $author
+     *
+     * @return Article
+     */
+    public function setAuthor(User $author)
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    /**
+     * Get author
+     *
+     * @return User
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    public function addCategory(Category $category)
+    {
+        if (!$this->categories->contains($category)){
+            $this->categories[] = $category;
+        }
+        return $this;
+    }
+
+    /**
+     * @param Category $category
+     */
+    public function removeCategory(Category $category)
+    {
+        $this->categories->removeElement($category);
+    }
+
+
+    public function getCategories()
+    {
+        return $this->categories;
+    }
+}
---
+++ src/AppBundle/Entity/CaseApp.php
@@ -0,0 +1,158 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Caseapp
+ *
+ * @ORM\Table(name="caseapp")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\CaseAppRepository")
+ */
+class CaseApp
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="site_name", type="string", length=100)
+     */
+    private $siteName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="site_pic", type="string", nullable=true)
+     */
+    private $sitePic;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="site_link", type="string", length=50)
+     */
+    private $siteLink;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="details", type="text", nullable=true)
+     */
+    private $details;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set siteName
+     *
+     * @param string $siteName
+     *
+     * @return CaseApp
+     */
+    public function setSiteName($siteName)
+    {
+        $this->siteName = $siteName;
+
+        return $this;
+    }
+
+    /**
+     * Get siteName
+     *
+     * @return string
+     */
+    public function getSiteName()
+    {
+        return $this->siteName;
+    }
+
+    /**
+     * Set siteLink
+     *
+     * @param string $siteLink
+     *
+     * @return CaseApp
+     */
+    public function setSiteLink($siteLink)
+    {
+        $this->siteLink = $siteLink;
+
+        return $this;
+    }
+
+    /**
+     * Get siteLink
+     *
+     * @return string
+     */
+    public function getSiteLink()
+    {
+        return $this->siteLink;
+    }
+
+    /**
+     * Set details
+     *
+     * @param string $details
+     *
+     * @return CaseApp
+     */
+    public function setDetails($details)
+    {
+        $this->details = $details;
+
+        return $this;
+    }
+
+    /**
+     * Get details
+     *
+     * @return string
+     */
+    public function getDetails()
+    {
+        return $this->details;
+    }
+
+    /**
+     * Set sitePic
+     *
+     * @param string $sitePic
+     *
+     * @return CaseApp
+     */
+    public function setSitePic($sitePic)
+    {
+        $this->sitePic = $sitePic;
+
+        return $this;
+    }
+
+    /**
+     * Get sitePic
+     *
+     * @return string
+     */
+    public function getSitePic()
+    {
+        return $this->sitePic;
+    }
+}
