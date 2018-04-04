7f6634d68b695063ec0490ba8197a27abc739149
tomming <233238526@qq.com>
2018-04-04 10:43:50 +0800

    完成添加菜单功能

---
+++ src/AppBundle/Entity/Menu.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Menu
+ *
+ * @ORM\Table(name="menu")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\MenuRepository")
+ */
+class Menu
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=20)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="link", type="string", length=20)
+     */
+    private $link;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="string", length=200, nullable=true)
+     */
+    private $description;
+
+
+
+    /**
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Menu")
+     */
+    private $parentMenu;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Menu
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set link
+     *
+     * @param string $link
+     *
+     * @return Menu
+     */
+    public function setLink($link)
+    {
+        $this->link = $link;
+    
+        return $this;
+    }
+
+    /**
+     * Get link
+     *
+     * @return string
+     */
+    public function getLink()
+    {
+        return $this->link;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Menu
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    public function setParentMenu(Menu $parentmenu=null){
+        $this->parentMenu = $parentmenu;
+        return $this;
+    }
+
+    public function getParentMenu(){
+        return $this->parentMenu;
+    }
+
+    public function getParentMenuName(){
+        if(null == $this->parentMenu){
+            return '根菜单';
+        }
+        return $this->parentMenu->getName();
+    }
+
+
+}
+
